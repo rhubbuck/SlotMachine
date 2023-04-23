445fdf25df1738f2ceedfe4ab73ea1761df32278
Ryan Hubbuck <rhubbuck@gmail.com>
2023-04-23 13:20:21 -0400

    Update to reformat code and comments

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import random
 
 MAX_LINES = 3
